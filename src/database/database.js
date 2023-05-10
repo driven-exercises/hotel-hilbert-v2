b2151c51eed12de2d0e09bf92de7b04547a607fe
Letícia Alves Chijo <41638060+leticia-chijo@users.noreply.github.com>
2023-05-10 12:25:07 -0300

    Fix: connectionString com role do bootcamp

--- src/database/database.js
+++ src/database/database.js
@@ -3,7 +3,7 @@ const { Pool } = pg;
 
 const configDatabase = {
   connectionString:
-    "postgres://postgres:SUA_SENHA_TOP@localhost:5432/exercicio_join_encadeado_7a7ce1c6",
+    "postgres://bootcamp_role:senha_super_hiper_ultra_secreta_do_role_do_bootcamp@localhost:5432/exercicio_join_encadeado_7a7ce1c6",
 };
 
 export const db = new Pool(configDatabase);
