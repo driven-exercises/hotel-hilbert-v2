ea8c5208e1da0cb4466df7749b89860e0e08f47f
Letícia Alves Chijo <41638060+leticia-chijo@users.noreply.github.com>
2023-05-11 17:40:31 -0300

    Comandos SQL maiúsculos

--- src/app.js
+++ src/app.js
@@ -7,26 +7,21 @@ app.use(express.json());
 
 app.get("/allocations", async (req, res) => {
   try {
-    // busque o histórico de hospedagens, junto das informações do cliente e do quarto
-    const resultGuests = await db.query("select * from guests");
-    const resultRooms = await db.query("select * from rooms");
-    const resultAllocations = await db.query("select * from allocations");
+    const resultGuests = await db.query(`SELECT * FROM guests;`)
+    const resultRooms = await db.query(`SELECT * FROM rooms;`)
+    const resultAllocations = await db.query(`SELECT * FROM allocations;`)
 
-    const allocations = resultAllocations.rows.map((allocation) => ({
+    const allocations = resultAllocations.rows.map(allocation => ({
       ...allocation,
-      guestName: resultGuests.rows.find(
-        (guest) => guest.id === allocation.guestId
-      ).name,
-      roomName: resultRooms.rows.find((room) => room.id === allocation.roomId)
-        .name,
-    }));
+      guestName: resultGuests.rows.find(g => g.id === allocation.guestId).name,
+      roomName: resultRooms.rows.find(r => r.id === allocation.roomId).name
+    }))
 
-    res.send(allocations);
+    res.send(allocations)
   } catch (err) {
-    console.error(err);
-    res.sendStatus(500);
+    res.status(500).send(err.message)
   }
-});
+})
 
 app.listen(4000, () => {
   console.log("Server is listening on port 4000.");
